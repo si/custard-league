a15b06f164706a428c322541ac89a38e6647bf39
Simon Jobling <simon.jobling@gmail.com>
2014-02-18 17:32:27 +0000

    New customised Pool Game form with UserScroll UI (similar to arcade player selection UI).

--- app/Controller/PoolGamesController.php
+++ app/Controller/PoolGamesController.php
@@ -1,81 +1,84 @@
 <?php
 class PoolGamesController extends AppController {
 
-	var $scaffold;
-	
-	function beforeRender() {
+  var $scaffold;
+
+  function beforeRender() {
     parent::beforeRender();
   }
 
-	function beforeFilter() {
+  function beforeFilter() {
     parent::beforeFilter();
   }
 
-	public function index() {
+  public function index() {
 
-		$conditions = array();
+    $conditions = array();
 
-		$rankings = $this->PoolGame->find('all', array(
-		'fields' => array(
-			'Winner.first_name',
-			'Winner.team_id',
-			'COUNT(winner) wins',
-			'(SELECT COUNT(*) FROM pool_games WHERE player_1 = Winner.id OR player_2 = Winner.id) total_played',
-			'(COUNT(winner)/(SELECT COUNT(*) FROM pool_games WHERE player_1 = Winner.id OR player_2 = Winner.id)) win_ratio'
-		),
-/*
+    $rankings = $this->PoolGame->find('all', array(
+        'fields' => array(
+          'Winner.first_name',
+          'Winner.team_id',
+          'COUNT(winner) wins',
+          '(SELECT COUNT(*) FROM pool_games WHERE player_1 = Winner.id OR player_2 = Winner.id) total_played',
+          '(COUNT(winner)/(SELECT COUNT(*) FROM pool_games WHERE player_1 = Winner.id OR player_2 = Winner.id)) win_ratio'
+        ),
+        /*
 		'where' => array(
 		  'PoolGames.created > CURDATE()-14',
 		),
 
-*/		'group' => array(
-			'winner',
-		),
-		'order' => array(
-			'win_ratio DESC',
-		),
-		));
+*/  'group' => array(
+          'winner',
+        ),
+        'order' => array(
+          'win_ratio DESC',
+        ),
+      ));
 
-		$this->set('rankings', $rankings);
+    $this->set('rankings', $rankings);
 
-		$this->set('poolGames', $this->PoolGame->find('all', array(
-			'conditions'=>array(),
-			'limit' => 10,
-			'order' => array(
-				'PoolGame.created DESC',
-			),
-		)));
+    $this->set('poolGames', $this->PoolGame->find('all', array(
+          'conditions'=>array(),
+          'limit' => 10,
+          'order' => array(
+            'PoolGame.created DESC',
+          ),
+        )));
 
-	}
-
-	public function form($id='') {
-		
-		if(isset($this->data['PoolGame'])) {
-			var_dump($this->data);
-			$this->data['PoolGame']['winner'] = $this->data['PoolGame']['player_' . $this->data['PoolGame']['winner']];
-			echo 'Winner is ' . $this->data['PoolGame']['winner'];
-		}
-		
-		if($id!='') {
-			$conditions = array();
-			$conditions['id'] = $id;
-			$poolGame = $this->PoolGame->find('first', array('conditions'=>$conditions));
-			$this->set('poolGame', $poolGame);
-		}
-		$this->set('players', $this->PoolGame->Player1->find('list'));
-		$this->set('players_extra', $this->PoolGame->Player1->find('all',array(
-		  'fields'=>array('id','first_name','avatar')
-		  ,'conditions'=>array('disabled IS NULL')
-		)));
   }
-  
+
+  public function form($id='') {
+
+    if(isset($this->data['PoolGame'])) {
+//      var_dump($this->data);
+      $data = $this->data;
+      $data['PoolGame']['winner'] = $data['PoolGame'][('player_' . $data['PoolGame']['winner'])];
+//      echo 'Winner is ' . $data['PoolGame']['winner'];
+      $this->PoolGame->save($data);
+      $this->redirect( array('action'=>'view',$this->PoolGame->id) );
+    }
+
+    if($id!='') {
+      $conditions = array();
+      $conditions['id'] = $id;
+      $poolGame = $this->PoolGame->find('first', array('conditions'=>$conditions));
+      $this->set('poolGame', $poolGame);
+    }
+    $this->set('players', $this->PoolGame->Player1->find('list'));
+    $this->set('players_extra', $this->PoolGame->Player1->find('all',array(
+          'fields'=>array('id','first_name','avatar')
+          ,'conditions'=>array('disabled IS NULL')
+        )));
+  }
+
   public function view($id='') {
 
-		if($id!='') {
-			$poolGame = $this->PoolGame->findById($id);
-			$this->set('poolGame', $poolGame);
-		}
+    if($id!='') {
+      $poolGame = $this->PoolGame->findById($id);
+      $this->set('poolGame', $poolGame);
+    }
 
-	}
+  }
 
 }
--- app/View/Elements/pool_game_form.ctp
+++ app/View/Elements/pool_game_form.ctp
@@ -11,8 +11,8 @@ if(isset($poolGame)) echo $this->Form->input('id', $poolGame['PoolGame']['id']);
   <?php foreach($players_extra as $player) :   ?>
     <label>
       <input type="radio" 
-        name="player_1" 
-        id="player_1_<?php echo $player['Player1']['id']; ?>" 
+        name="data[PoolGame][player_1]" 
+        id="PoolGamePlayer1<?php echo $player['Player1']['id']; ?>" 
         value="<?php echo $player['Player1']['id']; ?>" />
       <img src="/img/avatars/<?php echo ($player['Player1']['avatar']!='') ? $player['Player1']['avatar'] : 'unknown.jpg'; ?>" alt="" />
       <span><?php echo $player['Player1']['first_name']; ?></span>
@@ -27,8 +27,8 @@ if(isset($poolGame)) echo $this->Form->input('id', $poolGame['PoolGame']['id']);
   <?php foreach($players_extra as $player) :   ?>
     <label>
       <input type="radio" 
-        name="player_2" 
-        id="player_2_<?php echo $player['Player1']['id']; ?>" 
+        name="data[PoolGame][player_2]" 
+        id="PoolGamePlayer<?php echo $player['Player1']['id']; ?>" 
         value="<?php echo $player['Player1']['id']; ?>" />
       <img src="/img/avatars/<?php echo ($player['Player1']['avatar']!='') ? $player['Player1']['avatar'] : 'unknown.jpg'; ?>" alt="" />
       <span><?php echo $player['Player1']['first_name']; ?></span>
@@ -36,13 +36,10 @@ if(isset($poolGame)) echo $this->Form->input('id', $poolGame['PoolGame']['id']);
   <?php endforeach; ?>
     </div>
   </fieldset>
-  
-  <?php // echo $this->Form->input('player_1',array('type'=>'radio', 'options'=>$players, 'div'=>'user-select', 'separator'=>'IMG')); ?>
-  <?php // echo $this->Form->input('player_2',array('type'=>'radio', 'options'=>$players, 'div'=>'user-select')); ?>
-  
+    
   <?php echo $this->Form->input('winner', array('options'=>array(1=>'Player 1',2=>'Player 2'), 'type' => 'radio')); ?>    
   
-  <?php echo $this->Form->button('Save Game', array('class'=>'big')); ?>
+  <?php echo $this->Form->button('Save Game', array('class'=>'cta')); ?>
 
 </fieldset>
 
