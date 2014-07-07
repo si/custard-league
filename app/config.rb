38669fbd45e19c471c77e98cbf7ac2f7d1652643
Si Jobling <simon.jobling@gmail.com>
2014-07-07 18:13:54 +0100

    Ruby config at app level
    Only really for LiveReload app

--- app/config.rb
+++ app/config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "webroot/css"
+sass_dir = "webroot/sass"
+images_dir = "webroot/img"
+javascripts_dir = "webroot/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
